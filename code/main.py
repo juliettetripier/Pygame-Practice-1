7eb22497c205706eb593cf5f372dcef893092b65
Juliette Tripier <juliette@Juliettes-Air.attlocal.net>
2023-12-29 08:54:02 -0800

    Displayed score on screen and added simple  game over screen.

--- code/main.py
+++ code/main.py
@@ -2,6 +2,15 @@ import pygame, sys
 from settings import *
 
 class Game:
+    def display_score(self):
+        current_time = int(pygame.time.get_ticks() / 1000) - self.start_time
+        score_surface = self.test_font.render(f'Score: {current_time}',False,(64,64,64))
+        score_rect = score_surface.get_rect(center = (400, 50))
+        score_bg = score_rect.inflate(10,10)
+        pygame.draw.rect(self.screen, '#c0e8ec', score_bg)
+        self.screen.blit(score_surface, score_rect)
+        return current_time
+
     def __init__(self):
 
         pygame.init() #starts pygame
@@ -14,7 +23,9 @@ class Game:
         #set up font - pygame.font.Font(font type, font size)
         self.test_font = pygame.font.Font('../font/Pixeltype.ttf', 50)
 
-        self.game_active = True
+        self.game_active = False
+
+        self.start_time = 0
 
         self.sky_surface = pygame.image.load('../graphics/Sky.png').convert()
         self.ground_surface = pygame.image.load('../graphics/ground.png').convert()
@@ -27,10 +38,16 @@ class Game:
         self.player_rect = self.player_surface.get_rect(midbottom = (80,300))
         self.player_gravity = 0
 
-        #for rendering fonts: render(text info, anti-alias boolean, color)
-        self.score_surface = self.test_font.render('My game', False, (64,64,64))
-        self.score_rect = self.score_surface.get_rect(center = (400, 50))
-        self.score_bg = self.score_rect.inflate(10,10)
+        # Intro screen
+        self.player_stand = pygame.image.load('../graphics/player/player_stand.png').convert_alpha()
+        self.player_stand = pygame.transform.rotozoom(self.player_stand, 0, 2)
+        self.player_stand_rect = self.player_stand.get_rect(center = (400,200))
+
+        self.game_name = self.test_font.render('Pixel Runner', False, (111,196,169))
+        self.game_name_rect = self.game_name.get_rect(center = (400, 80))
+
+        self.game_message = self.test_font.render('Press space to run', False, (111,196,169))
+        self.game_message_rect = self.game_message.get_rect(center = (400, 330))
     
 
     def run(self):
@@ -55,6 +72,8 @@ class Game:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_SPACE:
                             self.game_active = True
+                            self.snail_rect.left = 800
+                            self.start_time = int(pygame.time.get_ticks() / 1000)
             
             if self.game_active:
                 # Background
@@ -64,13 +83,10 @@ class Game:
                 self.screen.blit(self.ground_surface, (0, 300))
 
                 # Score
-                #draw a rectangle background behind the score text (display surface, color, rectangle)
-                # pygame.draw.rect(self.screen, '#c0e8ec', self.score_rect)
-                pygame.draw.rect(self.screen, '#c0e8ec', self.score_bg)
-                self.screen.blit(self.score_surface, self.score_rect)
+                self.display_score()
 
                 # Snail
-                self.snail_rect.x -= 4
+                self.snail_rect.x -= 6
                 if self.snail_rect.right < 0:
                     self.snail_rect.left = 800
                 self.screen.blit(self.snail_surface, self.snail_rect)
@@ -87,7 +103,10 @@ class Game:
                     self.game_active = False
                 
             else:
-                self.screen.fill('black')
+                self.screen.fill((94, 129, 162))
+                self.screen.blit(self.player_stand, self.player_stand_rect)
+                self.screen.blit(self.game_name, self.game_name_rect)
+                self.screen.blit(self.game_message, self.game_message_rect)
 
             #update the display surface so anything drawn inside the while loop is displayed
             pygame.display.update()
